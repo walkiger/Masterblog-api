bdafdc5c4e698aa97d4fb4bd0ce5817c794d5b94
walkiger <116552530+walkiger@users.noreply.github.com>
2025-01-20 18:33:01 +0100

    added functionality to add posts via api

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,13 +9,34 @@ POSTS = [
     {"id": 2, "title": "Second Post", "content": "This is the second post."},
 ]
 
-@app.route('/')
-def home():
-    return 'Welcome to the Blog API. Use /api/posts to get the list of posts.'
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
+
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    data = request.get_json()
+
+    if not data or 'title' not in data or 'content' not in data:
+        return jsonify({"error": "Title and content are required."}), 400
+
+    new_id = max(post['id'] for post in POSTS) + 1 if POSTS else 1
+    new_post = {
+        "id": new_id,
+        "title": data['title'],
+        "content": data['content']
+    }
+    POSTS.append(new_post)
+
+    return jsonify(new_post), 201
+
+
+@app.route('/')
+def home():
+    return 'Welcome to the Blog API. Use /api/posts to get the list of posts.'
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
