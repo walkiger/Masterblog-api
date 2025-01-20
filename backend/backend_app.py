487120cb402c6f839f2b52bef8af9fc808b372df
walkiger <116552530+walkiger@users.noreply.github.com>
2025-01-20 18:00:39 +0100

    added home route to display welcome message

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -5,15 +5,17 @@ app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
 POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
+    {"id": 1, "title": "First Post", "content": "This is the first post."},
+    {"id": 2, "title": "Second Post", "content": "This is the second post."},
 ]
 
+@app.route('/')
+def home():
+    return 'Welcome to the Blog API. Use /api/posts to get the list of posts.'
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
     return jsonify(POSTS)
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
